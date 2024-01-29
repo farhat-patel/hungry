b9c244db632df66b03657c84a5a457f389b784f0
ayeshapatel1612 <ayesha.patel@decimalpointanalytics.com>
2024-01-30 00:39:56 +0530

    Made Body Component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Favourites from "./components/Favourites";
+import Body from "./components/Body";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<Header />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/favourites" element={<Favourites />} />
-        </Routes>
-      </div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Body />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/favourites" element={<Favourites />} />
+      </Routes>
     </Router>
   );
 }
---
+++ src/components/Body.js
@@ -0,0 +1,14 @@
+import FryingPan from "./FryingPan";
+
+const Body = () => {
+  return (
+    <div>
+      <p className="text-2xl lg:text-4xl text-center font-semibold p-20">
+        Nothing to show, please search anything!
+      </p>
+      <FryingPan />
+    </div>
+  );
+};
+
+export default Body;
